b69945e41937ce92e70c9a45360f2cf62ad72a58
OliverBelfiore <olivermbelfiore@gmail.com>
2025-04-28 23:22:49 +0200

    practice: first 3 javascript practices are done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+  let arr = [];
+  if (times < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < times; i++) {
+    arr.push(string);
+  }
+  return arr.join("");
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let reversed_str = [];
 
+  for (let i = str.length; i >= 0; i--) {
+    reversed_str.push(str[i]);
+  }
+
+  return reversed_str.join("");
 };
 
 // Do not edit below this line
