668a5fa67fd763d9d3facea029507eee7fc01dcb
OliverBelfiore <olivermbelfiore@gmail.com>
2025-04-29 22:41:05 +0200

    test: 05 Pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (start_num, end_num) {
+  let sum = 0;
+  if (start_num < 0 || end_num < 0) {
+    return "ERROR";
+  }
+  if (!Number.isInteger(start_num) || !Number.isInteger(end_num)) {
+    return "ERROR";
+  }
+  const bigNumber = start_num > end_num ? start_num : end_num;
+  const smallNumber = start_num > end_num ? end_num : start_num;
 
+  for (let i = smallNumber; i <= bigNumber; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
