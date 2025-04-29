6e7b07fc60cdaa5d15ad9e1ab52f922b856118ab
OliverBelfiore <olivermbelfiore@gmail.com>
2025-04-29 22:57:57 +0200

    test: 07 solved
    toFixed is better than toPrecision for decimals. But they both return
    str !??!?

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const conv_temp = (temp - 32) * (5 / 9);
+  return Number(conv_temp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const conv_temp = (temp * 9) / 5 + 32;
+  return Number(conv_temp.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
