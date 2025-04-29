fc5998e7bd1e187ca32f70829fd1028d2217f509
OliverBelfiore <olivermbelfiore@gmail.com>
2025-04-29 22:49:05 +0200

    test: 06 don

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  // leap years are divisible by 4 unless they are divisible by 100 unless they are divisible by 400
+  if (year % 4 == 0) {
+    if (year % 100 != 0 || year % 400 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
