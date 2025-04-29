d09b3618564735080f99df1ab52aaa783db99371
OliverBelfiore <olivermbelfiore@gmail.com>
2025-04-29 22:29:46 +0200

    task: 04 passing all test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...num_to_remove) {
+  // Now the num_to_remove can be a an array, i.e. similar to the kwargs in python
+  // Then we filter the array from numers that are not included in the num_to_remove array
+  let clean_array = arr.filter((num) => !num_to_remove.includes(num));
+
+  return clean_array;
 };
 
 // Do not edit below this line
