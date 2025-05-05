3a3ee7733a8fc4e5fe9d7509893f4a17a72102a8
OliverBelfiore <olivermbelfiore@gmail.com>
2025-05-05 20:14:56 +0200

    test: Too easy

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
+const getTheTitles = function (book_arr) {
+  let list_titles = [];
 
+  for (const book of book_arr) {
+    list_titles.push(extract_title(book));
+  }
+  return list_titles;
 };
+
+function extract_title(book) {
+  return book["title"];
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
