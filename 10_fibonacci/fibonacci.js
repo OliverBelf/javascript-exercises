c5426775eadca68fd674f446e29d4cbd6dc43e4e
OliverBelfiore <olivermbelfiore@gmail.com>
2025-05-05 20:07:29 +0200

    test: Rookie Fib numbers but working

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (!Number.isInteger(num)) {
+    num = parseInt(num);
+  }
+  let fib_number = 1;
+  let nums = [1];
 
+  if (num < 0) {
+    return "OOPS";
+  } else if (num == 0) {
+    return 0;
+  }
+
+  for (let i = 0; i < num; i++) {
+    nums.push(fib_number);
+    fib_number += nums[i];
+  }
+
+  return nums[num - 1];
 };
 
 // Do not edit below this line
