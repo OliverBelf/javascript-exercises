b569830b6254004b379fecc7e911cb41c18b2f75
OliverBelfiore <olivermbelfiore@gmail.com>
2025-05-05 20:25:53 +0200

    test: Final test pass, this was too easy also but quite fun

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
+const findTheOldest = function (people_obj) {
+  // loop through the people obj
+  // calculate the age
+  // Store the oldest person obj
+  // return people_obj[idx]
 
+  let oldest_person;
+  let oldest_age = 0;
+  let new_age;
+  for (const person of people_obj) {
+    // This gives the person object
+
+    new_age = find_age(person);
+    if (new_age > oldest_age) {
+      oldest_age = new_age;
+      oldest_person = person;
+    }
+  }
+  return oldest_person;
 };
+
+function find_age(person) {
+  const birth_year = person.yearOfBirth;
+  const death_year = person.yearOfDeath;
+  let age;
+
+  if (death_year == undefined) {
+    const currentYear = new Date().getFullYear();
+    age = currentYear - birth_year;
+  } else {
+    age = death_year - birth_year;
+  }
+
+  return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
