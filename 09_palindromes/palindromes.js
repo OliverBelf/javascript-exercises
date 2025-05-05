c5d4779f16c4b134810ea81529f7c31da5b4393c
OliverBelfiore <olivermbelfiore@gmail.com>
2025-05-05 19:50:04 +0200

    test: passing the 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  str = str.toLowerCase();
+  char_list = [];
+  char_list_foward = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i].match(/[a-z0-9]/i)) {
+      char_list.push(str[i]);
+    }
+  }
 
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].match(/[a-z0-9]/i)) {
+      char_list_foward.push(str[i]);
+    }
+  }
+  console.log(char_list.join(""));
+  console.log(char_list_foward.join(""));
+
+  if (char_list.join("") == char_list_foward.join("")) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
